49d415f78511df0c624f53b8f123e4084ab88574
Sameer Puri <sameer@users.noreply.github.com>
2021-04-16 02:41:41 -0400

    fully automate apartments.js

--- README.md
+++ README.md
@@ -14,13 +14,11 @@ I pulled apartments in Culver city along with the Metro stations:
 
 ## Instructions
 
-1. Do your search on apartments.com
-1. Create a file called `apartments.tsv`. In that file, put `name	address` as the first line.
+1. Do your search on apartments.com. Make sure you are on page 1 of the results.
 1. Right click on the page and hit "Inspect Element". In Chrome, this is just "Inspect".
     * Beware that this can change the size of the map and the results may change.
 1. Go to the tab that says "Console".
-1. Copy and paste apartments.js into the console. This will give you the first 25 listings on the page in TSV format.
-1. Copy and paste that into your `apartments.tsv`.
-1. Scroll down to the bottom of the listings and click the next page button.
-1. Repeat steps page by page until you have all the listings.
-1. Upload to Google My Maps.
+1. Copy and paste apartments.js into the console. When it's done, the page will offer to save an `apartments.tsv` file. Download that.
+1. Upload it to Google My Maps.
+1. Pick address for the positioning column.
+1. Pick name for the marker name column.
--- apartments.js
+++ apartments.js
@@ -1,11 +1,35 @@
 // apartments.com listings
-console.log(Array.from(document.querySelectorAll('article.placard-option')).map(propertyInfo => {
-    const titleElement = propertyInfo.querySelector('span.title');
-    if (titleElement == null) {
-        console.log(propertyInfo);
+(async () => {
+    console.log('Please wait while I click through the pages...')
+    let tsv = 'name\taddress\tURL\n';
+    let nextButton = document.querySelector('a.next ');
+    for(;;) {
+        tsv += Array.from(document.querySelectorAll('article.placard-option')).map(propertyInfo => {
+            const titleElement = propertyInfo.querySelector('span.title');
+            const title = titleElement.textContent;
+            const addressElement = propertyInfo.querySelector('div.property-address');
+            const address = addressElement.getAttribute('title');
+            const url = propertyInfo.querySelector('a.property-link').href;
+            return title + '\t' + address + '\t' + url + '\n'
+        }).reduce((acc, curr) => acc + curr, '');
+        if (nextButton != null) {
+            const prevTitle = document.querySelector('span.title').textContent;
+            nextButton.click();
+            do {
+                await new Promise(r => setTimeout(r, 500));
+            } while (prevTitle === document.querySelector('span.title').textContent);
+            if (nextButton == document.querySelector('a.next ')) {
+                console.log('could not click');
+                break;
+            }
+            nextButton = document.querySelector('a.next ');
+        } else {
+            break;
+        }
     }
-    const title = titleElement.textContent;
-    const addressElement = propertyInfo.querySelector('div.property-address');
-    const address = addressElement.getAttribute('title');
-    return title + '\t' + address + '\n'
-}).reduce((acc, curr) => acc + curr, ''));
+    const tsvDataUri = 'data:text/tsv;base64,' + btoa(tsv);
+    const link = document.createElement('a');
+    link.href = tsvDataUri;
+    link.download = 'apartments.tsv';
+    link.click();
+})();
