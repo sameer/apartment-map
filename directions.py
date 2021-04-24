45f94c257c8be15512fff7889653ffc61bf6663c
Sameer Puri <sameer@users.noreply.github.com>
2021-04-24 18:41:34 -0400

    add departure time

--- directions.py
+++ directions.py
@@ -2,13 +2,20 @@
 import googlemaps
 import pandas
 import os
+from datetime import datetime, timezone, timedelta
 
 OFFICE = 'Put your office here'
+# Maximum time you're willing to wait
+MAX_DIRECTIONS_DURATION_SECS = 60 * 15
+TIMEZONE = timezone(timedelta(hours=-7))
+# When you leave for work i.e. 7:15 AM on a Monday
+DEPARTURE_TIME = str(int(datetime(2021, 4, 26, 7, 15, 00, tzinfo=TIMEZONE).timestamp()))
+
+
 gmaps = googlemaps.Client(key=os.environ['API_KEY'])
 
 apartments_df = pandas.read_csv("apartments.tsv", delimiter='\t')
 apartment_addresses = apartments_df.address.tolist()
-
 distance = []
 duration = []
 fare = []
@@ -18,7 +25,8 @@ for apartment_address in apartment_addresses:
         apartment_address,
         mode='transit',
         transit_mode='rail',
-        traffic_model='pessimistic'
+        traffic_model='pessimistic',
+        departure_time=DEPARTURE_TIME
     )
     distance_matrix_row_element = distance_matrix_response['rows'][0]['elements'][0]
     
@@ -37,6 +45,8 @@ for apartment_address in apartment_addresses:
     print(distance[-1], duration[-1], fare[-1])
 
 apartments_df['distance'] = distance
-apartments_df['duration'] = duration
+apartments_df['duration_mins'] = apartments_df['duration'] / 60
 apartments_df['fare'] = fare
+apartments_df = apartments_df[apartments_df['duration'] < MAX_DIRECTIONS_DURATION_SECS]
+apartments_df = apartments_df.sort_values(by='duration')
 apartments_df.to_csv('apartments-with-distance.tsv', sep='\t', index=False)
