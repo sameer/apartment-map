b286838465fa0ff712385161ef95bc91a07c3323
Sameer Puri <11097096+sameer@users.noreply.github.com>
2021-05-20 19:51:02 -0400

    Add clearer logging edge case handling
    * Strip non-ascii characters from response
    * Add error messages with info on how to get help
    * Use the title DOM element itself instead of its textual content to know when the next page is visible
    * Add clarifying comments

--- apartments.js
+++ apartments.js
@@ -3,7 +3,9 @@
     console.log('Please wait while I click through the pages...')
     let tsv = 'name\taddress\tURL\tprice-range\n';
     let nextButton = document.querySelector('a.next ');
+    let pageNumber = 1;
     for(;;) {
+        // add listings on page to TSV
         tsv += Array.from(document.querySelectorAll('article.placard-option')).map(propertyInfo => {
             const titleElement = propertyInfo.querySelector('span.title');
             const title = titleElement.textContent;
@@ -11,6 +13,7 @@
             const address = addressElement.getAttribute('title');
             const url = propertyInfo.querySelector('a.property-link').href;
             let priceRange = propertyInfo.querySelector('div.price-range');
+            // some listings do not show prices
             if (priceRange == null) {
                 priceRange = 'N/A';
             } else {
@@ -18,24 +21,34 @@
             }
             return title + '\t' + address + '\t' + url + '\t' + priceRange + '\n'
         }).reduce((acc, curr) => acc + curr, '');
+        // load next page or finish search
         if (nextButton != null) {
-            const prevTitle = document.querySelector('span.title').textContent;
+            const prevTitle = document.querySelector('span.title');
             nextButton.click();
+            pageNumber++;
             do {
-                await new Promise(r => setTimeout(r, 500));
-            } while (prevTitle === document.querySelector('span.title').textContent);
+                console.log('Waiting for page ' + pageNumber + ' to load');
+                await new Promise(r => setTimeout(r, 1000));
+            } while (prevTitle === document.querySelector('span.title'));
+            console.log('Processing page ' + pageNumber);
             if (nextButton == document.querySelector('a.next ')) {
-                console.log('could not click');
+                console.log('The next button seems to be the same button after I clicked it.\nEither your browser does not allow programmatic clicks or Apartments.com changed their website.\nPlease create an issue on GitHub.');
                 break;
             }
             nextButton = document.querySelector('a.next ');
         } else {
+            console.log('Done searching!');
             break;
         }
     }
+    // btoa does not support non-ascii characters: https://stackoverflow.com/q/23223718
+    tsv = tsv.replace(/[^\x00-\x7F]/g, "");
     const tsvDataUri = 'data:text/tsv;base64,' + btoa(tsv);
     const link = document.createElement('a');
     link.href = tsvDataUri;
     link.download = 'apartments.tsv';
     link.click();
-})();
+})().catch(e => {
+    console.log('There was an error while pulling listings: ' + e.message + '\nThere may be an issue with your browser or Apartments.com has changed their website. Please create an issue on GitHub.');
+    console.log(e.stack);
+});
