6ecb059eb18c435dad33f24f8d6bdecbd0ae9fb8
Sameer Puri <sameer@users.noreply.github.com>
2021-06-22 22:53:34 -0400

    handle bug in apartments.com where next button is missing

--- apartments.js
+++ apartments.js
@@ -2,10 +2,12 @@
 (async () => {
     console.log('Please wait while I click through the pages...')
     let tsv = 'name\taddress\tURL\tprice-range\n';
-    let nextButton = document.querySelector('a.next ');
+    const pages = Array.from(document.querySelectorAll('ol > li > a')).filter(elem => elem.hasAttribute('data-page'));
+    pages.sort((a, b) => Number(a.getAttribute('data-page')) - Number(b.getAttribute('data-page')));
     let pageNumber = 1;
     for(;;) {
         // add listings on page to TSV
+        console.log(`Processing page ${pageNumber} out of ${pages.length}`);
         tsv += Array.from(document.querySelectorAll('article.placard-option')).map(propertyInfo => {
             const titleElement = propertyInfo.querySelector('span.title');
             const title = titleElement.textContent;
@@ -22,20 +24,18 @@
             return title + '\t' + address + '\t' + url + '\t' + priceRange + '\n'
         }).reduce((acc, curr) => acc + curr, '');
         // load next page or finish search
-        if (nextButton != null) {
+        if (pageNumber < pages.length) {
             const prevTitle = document.querySelector('span.title');
-            nextButton.click();
+            pages[pageNumber].click();
             pageNumber++;
             do {
                 console.log('Waiting for page ' + pageNumber + ' to load');
                 await new Promise(r => setTimeout(r, 1000));
             } while (prevTitle === document.querySelector('span.title'));
-            console.log('Processing page ' + pageNumber);
-            if (nextButton == document.querySelector('a.next ')) {
+            if (pages[pageNumber - 1].className === 'active') {
                 console.log('The next button seems to be the same button after I clicked it.\nEither your browser does not allow programmatic clicks or Apartments.com changed their website.\nPlease create an issue on GitHub.');
                 break;
             }
-            nextButton = document.querySelector('a.next ');
         } else {
             console.log('Done!');
             break;
