38e2bf6f20cc26ed6280a739614e48cdb7d0c8a1
Sameer Puri <sameer@users.noreply.github.com>
2021-08-02 23:50:03 -0400

    refactor directions.py: support multiple POIs, easier config

--- directions.py
+++ directions.py
@@ -2,63 +2,105 @@
 import googlemaps
 import pandas
 import os
-from datetime import datetime, timezone, timedelta
+from pytz import timezone
+from datetime import date, datetime, time, timedelta
+import logging
 
-OFFICES = ['2021 7th Ave, Seattle, WA 98121', '1209 124th Ave NE, Bellevue, WA 98005']
-# Maximum time you're willing to transit, including walking
+logging.basicConfig(level=logging.INFO)
+
+# Places you want to know how far the apartment is from (i.e. the office)
+POINTS_OF_INTEREST = [
+
+]
+# Maximum time you're willing to transit including walking
 MAX_DIRECTIONS_DURATION_MINS = 50
-TIMEZONE = timezone(timedelta(hours=-8))
-# When you arrive at work i.e. 8:00 AM on a Monday
-ARRIVAL_TIME = str(int(datetime(2021, 8, 2, 9, 00, 00, tzinfo=TIMEZONE).timestamp()))
 
-gmaps = googlemaps.Client(key=os.environ['API_KEY'])
+# Pacific Timezone
+TIMEZONE = timezone("US/Pacific")
+# When you arrive at work i.e. by 9:00 AM
+ARRIVAL_TIME = time(hour=9)
+# Day of week, 0 = Monday
+DAY_OF_WEEK = 0
 
-apartments_df = pandas.read_csv("apartments.tsv", delimiter='\t')
+ARRIVAL_DATE = datetime.today() + timedelta(
+    days=(DAY_OF_WEEK - date.today().weekday() + 7) % 7
+)
+ARRIVAL_DATE_TIME = TIMEZONE.localize(
+    datetime.combine(date=ARRIVAL_DATE, time=ARRIVAL_TIME)
+)
+ARRIVAL_TIMESTAMP = str(int(ARRIVAL_DATE_TIME.timestamp()))
+
+# See options at https://googlemaps.github.io/google-maps-services-python/docs/index.html#googlemaps.Client.distance_matrix
+TRANSIT_OPTS = {
+    "mode": "transit",
+    "traffic_model": "best_guess",
+    "transit_routing_preference": "fewer_transfers",
+    "arrival_time": ARRIVAL_TIMESTAMP,
+}
+
+gmaps = googlemaps.Client(key=os.environ["API_KEY"])
+
+apartments_df = pandas.read_csv("apartments.tsv", delimiter="\t")
 apartment_addresses = apartments_df.address.tolist()
 
-def chunked(seq, size):
-    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
-for i, office in enumerate(OFFICES):
+def chunked(seq, size):
+    return (seq[pos : pos + size] for pos in range(0, len(seq), size))
+
+
+for i, poi in enumerate(POINTS_OF_INTEREST):
     distance = []
     duration = []
     fare = []
     for chunk in chunked(list(enumerate(apartment_addresses)), 25):
         distance_matrix_response = gmaps.distance_matrix(
-            [address for _, address in chunk],
-            office,
-            mode='transit',
-            traffic_model='best_guess',
-            transit_routing_preference='fewer_transfers',
-            region='us',
-            arrival_time=ARRIVAL_TIME
+            [address for _, address in chunk], poi, **TRANSIT_OPTS
         )
 
         for j in range(len(chunk)):
-            distance_matrix_row_element = distance_matrix_response['rows'][j]['elements'][0]
+            distance_matrix_row_element = distance_matrix_response["rows"][j][
+                "elements"
+            ][0]
             try:
-                distance.append(distance_matrix_row_element['distance']['value'])
+                distance.append(distance_matrix_row_element["distance"]["value"])
             except KeyError:
-                distance.append('N/A')
+                distance.append("N/A")
             try:
-                duration.append(int(distance_matrix_row_element['duration']['value']) / 60)
+                duration.append(
+                    int(distance_matrix_row_element["duration"]["value"]) / 60
+                )
             except KeyError:
                 # It's not a proper address or it's impossible to get to by transit
+                logging.warn(f"Missing directions from {chunk[j]} to {poi}")
                 duration.append(MAX_DIRECTIONS_DURATION_MINS + 1)
             try:
-                fare.append(distance_matrix_row_element['fare']['text'])
+                fare.append(distance_matrix_row_element["fare"]["text"])
             except KeyError:
-                fare.append('N/A')
-            print(f"{chunk[j][0]+1}/{len(apartment_addresses)}", distance[-1], duration[-1], fare[-1])
-    apartments_df[f'distance_{i}'] = distance
-    apartments_df[f'duration_{i}'] = duration
-    apartments_df[f'fare_{i}'] = fare
+                fare.append("N/A")
+            logging.info(
+                f"Processed {chunk[j][0]+1}/{len(apartment_addresses)}"
+            )
+    apartments_df[f"distance_{i}"] = distance
+    apartments_df[f"duration_{i}"] = duration
+    apartments_df[f"fare_{i}"] = fare
 
-for i in range(len(OFFICES)):
-    apartments_df = apartments_df[apartments_df[f'duration_{i}'] < MAX_DIRECTIONS_DURATION_MINS]
-apartments_df['max_duration'] = apartments_df[[f'duration_{i}' for i in range(len(OFFICES))]].max(axis=1)
-apartments_df['mean_duration'] = apartments_df[[f'duration_{i}' for i in range(len(OFFICES))]].mean(axis=1)
 
-print(f"Filtered down from {len(apartment_addresses)} to {len(apartments_df)}")
-apartments_df = apartments_df.sort_values(by='max_duration')
-apartments_df.to_csv('apartments-with-distance.tsv', sep='\t', index=False)
+apartments_df["max_duration"] = apartments_df[
+    [f"duration_{i}" for i in range(len(POINTS_OF_INTEREST))]
+].max(axis=1)
+
+
+apartments_df = apartments_df[
+    apartments_df["max_duration"] < MAX_DIRECTIONS_DURATION_MINS
+]
+
+apartments_df["min_duration"] = apartments_df[
+    [f"duration_{i}" for i in range(len(POINTS_OF_INTEREST))]
+].max(axis=1)
+apartments_df["mean_duration"] = apartments_df[
+    [f"duration_{i}" for i in range(len(POINTS_OF_INTEREST))]
+].mean(axis=1)
+
+logging.info(f"Filtered down from {len(apartment_addresses)} to {len(apartments_df)}")
+apartments_df = apartments_df.sort_values(by="max_duration")
+apartments_df.to_csv("apartments-with-distance.tsv", sep="\t", index=False)
